92281c82b050a20851e785671afed59e9b2ace42
Jery <jery99961@gmail.com>
2025-10-13 02:42:58 +0530

    feat: implement Genshin Impact widget with responsive design and enhanced user interface

--- api.js
+++ api.js
@@ -3,6 +3,17 @@ const express = require("express");
 const createAPI = (port = 8080) => {
 	const api = express();
 
+	// Enable CORS for all routes
+	api.use((req, res, next) => {
+		res.header("Access-Control-Allow-Origin", "*");
+		res.header("Access-Control-Allow-Methods", "GET, OPTIONS");
+		res.header("Access-Control-Allow-Headers", "Content-Type");
+		if (req.method === "OPTIONS") {
+			return res.sendStatus(200);
+		}
+		next();
+	});
+
 	api.get("/api/genshin/notes", async (req, res) => {
 		try {
 			const accounts = app.HoyoLab.getActiveAccounts({ whitelist: "genshin" });
